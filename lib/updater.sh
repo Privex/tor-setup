efaffdec7aa4ab71d95681c8d00f6f373af1c507
Chris (Someguy123) <info@someguy123.com>
2019-11-02 18:42:31 +0000

    0.8.5 run git reset hard before pulling

--- lib/updater.sh
+++ lib/updater.sh
@@ -23,6 +23,7 @@ update_torsetup() {
             msg yellow " [?] TorSetup install folder not writable by this user, but sudo appears to work."
             local git_url=$(sudo git remote get-url origin)
             msg yellow " [lib/updater.sh] (SUDO) Downloading TorSetup updates from Git: $git_url"
+            sudo git reset --hard
             sudo git pull -q -f > /dev/null
         else
             msg bold red " [!!!] The TorSetup installation folder '$TORSETUP_DIR' is not writable, and passwordless sudo is unavailable."
@@ -31,6 +32,7 @@ update_torsetup() {
     else
         local git_url=$(git remote get-url origin)
         msg yellow "[lib/updater.sh] Downloading TorSetup updates from Git: $git_url"
+        git reset --hard
         git pull -q -f > /dev/null
     fi
     msg
